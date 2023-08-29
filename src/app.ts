6cc9cfc86f9cc43c11fb2bf7e366bc580b6a7472
m4zeboy <moises775azevedo@gmail.com>
2023-08-29 16:01:15 -0300

    Lidando com erros do use case

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
@@ -7,3 +9,19 @@ export const app = fastify()
 // D - Dependency Inversion Principle
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO Here we should log to an external tool like DATADOG
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})

69179b83fb41977ef86dac4dc940f94ecd3cc8e6
m4zeboy <moises775azevedo@gmail.com>
2023-08-31 16:13:01 -0300

    Testes e controller de autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  // pense que cada rota é uma entidade post /user -> 'criar um usuário'
+  // post /authenticate não faz muito sentido -> 'criar um autenticar'
+  // post /sessions é mais legível -> 'criar uma sessão'
+  app.post('/sessions', authenticate)
 }

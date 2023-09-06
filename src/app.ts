8b2fe966b129baef718e3c41aca2ad1bf1b25c81
m4zeboy <moises775azevedo@gmail.com>
2023-09-06 17:09:33 -0300

    Implementando JWT no Fastify

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
 
 export const app = fastify()
 
 // SOLID
 // D - Dependency Inversion Principle
-
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 app.register(appRoutes)
 
 app.setErrorHandler((error, _, reply) => {
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
@@ -8,4 +9,6 @@ export async function appRoutes(app: FastifyInstance) {
   // post /authenticate não faz muito sentido -> 'criar um autenticar'
   // post /sessions é mais legível -> 'criar uma sessão'
   app.post('/sessions', authenticate)
+  /* AUTHENTICATED */
+  app.get('/me', profile)
 }

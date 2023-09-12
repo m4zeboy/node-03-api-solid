56824a5de78bf7b77bea61f9c41856a30b8a2f59
m4zeboy <moises775azevedo@gmail.com>
2023-09-12 17:37:35 -0300

    Teste E2E do perfil

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,26 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  console.log('ok')
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password',
+    })
+    const response = await request(app.server).post('/sessions').send({
+      email: 'john.doe@example.com',
+      password: 'password',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({ token: expect.any(String) })
+  })
 })
